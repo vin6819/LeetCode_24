88ae5e3210e0d0189806b4524c4b300460d1dae9
vin6819 <114672994+vin6819@users.noreply.github.com>
2024-12-04 14:02:47 +0530

    Time: 124 ms (42.85%), Space: 61.1 MB (22.2%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.java
@@ -2,12 +2,16 @@ class Solution {
     public int getLargestOutlier(int[] nums) {
         int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
+        int num[] = new int[nums.length];
+        int j = num.length;
+        Arrays.sort(nums);
         for(int i: nums) {
             sum += i;
+            num[--j] = i;
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
         int res = Integer.MIN_VALUE;
-        for(int i: nums) {
+        for(int i: num) {
             int ol = sum - i;
             if(ol % 2 != 0) {
                 continue;
@@ -17,6 +21,7 @@ class Solution {
             map.put(i, map.get(i) - 1);
             if(map.getOrDefault(ol, -1) > 0) {
                 res = Math.max(i, res);
+                break;
             }
             map.put(i, map.get(i) + 1);
         }
