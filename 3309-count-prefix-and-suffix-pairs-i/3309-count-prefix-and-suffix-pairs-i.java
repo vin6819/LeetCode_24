0cdbf9a16648c46213a3e92ba211947357d01450
vin6819 <114672994+vin6819@users.noreply.github.com>
2025-01-08 09:49:47 +0530

    Time: 2 ms (100%), Space: 42.3 MB (58.62%) - LeetHub

---
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int res = 0;
+        for(int i = 0; i < words.length; i++) {
+            for(int j = i + 1; j < words.length; j++) {
+                if(words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
