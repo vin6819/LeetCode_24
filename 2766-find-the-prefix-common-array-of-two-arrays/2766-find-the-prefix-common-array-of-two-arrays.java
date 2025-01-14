d7b67c0d2e4bec1868f66baea94929e752f17212
vin6819 <114672994+vin6819@users.noreply.github.com>
2025-01-14 22:06:46 +0530

    Time: 1 ms (100%), Space: 45.3 MB (88.54%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -3,14 +3,13 @@ class Solution {
         int n = A.length;
         int f[] = new int[n + 1];
         int res[] = new int[n];
+        int c = 0;
         for(int i = 0; i < n; i++) {
-            f[A[i]]++;
-            f[B[i]]++;
-            int c = 0;
-            for(int j = 0; j <= n; j++) {
-                if(f[j] == 2) {
-                    c++;
-                }
+            if(++f[A[i]] == 2) {
+                c++;
+            }
+            if(++f[B[i]] == 2) {
+                c++;
             }
             res[i] = c;
         }
