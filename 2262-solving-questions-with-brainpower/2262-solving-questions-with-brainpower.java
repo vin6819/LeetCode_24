6312722f4e5aa9a916b328e938254748d29b8849
vin6819 <114672994+vin6819@users.noreply.github.com>
2025-04-01 13:45:30 +0530

    Time: 10 ms (42.86%), Space: 125.1 MB (28.93%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -0,0 +1,18 @@
+class Solution {
+    static long helper(int a[][], int i, long dp[]) {
+        if (i >= a.length) {
+            return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        dp[i] = Math.max(a[i][0] + helper(a, i + a[i][1] + 1, dp), helper(a, i + 1, dp));
+        return dp[i];
+    }
+    public long mostPoints(int[][] questions) {
+        long dp[] = new long[questions.length + 1];
+        Arrays.fill(dp, -1);
+        long res = helper(questions, 0, dp);
+        return res;
+    }
+}
